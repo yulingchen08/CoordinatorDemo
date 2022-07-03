83cafbf88b94d5ad1a68accfa50790251d189532
Eric Chen <eric.chen@gozeppelin.com>
2022-07-03 16:36:34 +0800

    Initial Commit

---
+++ CoordinatorDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoordinatorDemo
+//
+//  Created by 陳鈺翎 on 2022/7/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CoordinatorDemoUITests/CoordinatorDemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CoordinatorDemoUITestsLaunchTests.swift
+//  CoordinatorDemoUITests
+//
+//  Created by 陳鈺翎 on 2022/7/3.
+//
+
+import XCTest
+
+class CoordinatorDemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
